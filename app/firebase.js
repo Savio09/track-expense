d62fe90512b7d6f23db26e2a619bd6402ed4b79a
Savio09 <declanfortune5@gmail.com>
2024-09-05 00:37:49 -0700

    added environment variable to hide keys

--- app/firebase.js
+++ app/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC7ePQN6_03sH1apONfUcyB0DgnWuPObQc",
-  authDomain: "expense-tracker-f6810.firebaseapp.com",
-  projectId: "expense-tracker-f6810",
-  storageBucket: "expense-tracker-f6810.appspot.com",
-  messagingSenderId: "779751570125",
-  appId: "1:779751570125:web:4c308668efaad62baec784",
+  apiKey: process.env.NEXT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
